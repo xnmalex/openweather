3b5386ece686fa38a30ce221c3dce3ea9ec91a82
Alex Gui <xnmalex@gmail.com>
2020-10-28 10:50:46 +0800

    init kotlin project

---
+++ app/src/main/java/com/alexgui/openweather/ApiEndPoint.kt
@@ -0,0 +1,8 @@
+package com.alexgui.openweather
+
+class ApiEndPoint{
+    companion object {
+        private val BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
+        val API_KEY = "beca725a3e128eefc58234d44cd743f7";
+    }
+}
---
+++ app/src/main/java/com/alexgui/openweather/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.alexgui.openweather.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexgui.openweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
