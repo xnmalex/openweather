724000f88340902db494fddc375c5247da9ce1dc
Alex Gui <xnmalex@gmail.com>
2020-10-29 13:11:51 +0800

    fixes web service query path

--- app/src/main/java/com/alexgui/openweather/model/remote/WeatherService.kt
+++ app/src/main/java/com/alexgui/openweather/model/remote/WeatherService.kt
@@ -1,14 +1,15 @@
 package com.alexgui.openweather.model.remote
 
+import com.alexgui.openweather.ApiEndPoint
 import com.alexgui.openweather.model.data.WeatherResponse
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherService {
 
-    @GET("")
-    suspend fun getWeatherByZipCode() : Response<WeatherResponse>
+    @GET("forecast")
+    suspend fun getWeatherByZipCode(@Query("zip") zip:String?= "94040,us", @Query("appid") appid:String = ApiEndPoint.API_KEY) : Response<WeatherResponse>
 
     @GET("")
     suspend fun getWeatherByLatLng() : Response<WeatherResponse>
