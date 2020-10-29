3b224bdbb55df6efdd68fc05995aa44579a30141
Alex Gui <xnmalex@gmail.com>
2020-10-29 13:10:03 +0800

    updated retrofit version and api endpoint to fix bug

--- app/src/main/java/com/alexgui/openweather/ApiEndPoint.kt
+++ app/src/main/java/com/alexgui/openweather/ApiEndPoint.kt
@@ -1,8 +1,9 @@
 package com.alexgui.openweather
 
 class ApiEndPoint{
+    //http://api.openweathermap.org/data/2.5/forecast?zip=94040,us&appid=beca725a3e128eefc58234d44cd743f7
     companion object {
-        val BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
+        val BASE_URL = "http://api.openweathermap.org/data/2.5/";
         val API_KEY = "beca725a3e128eefc58234d44cd743f7";
     }
 }
