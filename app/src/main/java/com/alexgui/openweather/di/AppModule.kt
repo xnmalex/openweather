50b869a412948505d93b28093d453751c5d79082
Alex Gui <xnmalex@gmail.com>
2020-10-29 13:10:29 +0800

    added http logging interceptor for debugging

--- app/src/main/java/com/alexgui/openweather/di/AppModule.kt
+++ app/src/main/java/com/alexgui/openweather/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.alexgui.openweather.di
 
 import android.content.Context
+import android.util.Log
 import com.alexgui.openweather.ApiEndPoint
 import com.alexgui.openweather.model.local.AppDatabase
 import com.alexgui.openweather.model.local.WeatherDao
@@ -14,20 +15,34 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
 
+
     @Singleton
     @Provides
-    fun provideRetrofit(gson: Gson) : Retrofit = Retrofit.Builder()
-            .baseUrl(ApiEndPoint.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
+    fun provideRetrofit(): Retrofit {
+        //val cache = Cache(context.cacheDir!!, 5 * 1024 * 1024)
+        val interceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { Log.d("debug", it) }).apply { level = HttpLoggingInterceptor.Level.BODY }
+        val okHttpClient = OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
             .build()
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(ApiEndPoint.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(provideGson()))
+            .build()
+    }
 
     @Provides
     fun provideGson(): Gson = GsonBuilder().create()
